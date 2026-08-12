d82d49e35977f427f7fa6ce1d0f9b172e6dee3d1
TMino <t@mino.ltd>
2026-08-13 08:54:41 +0900

    perf(db): enable PDO persistent connections
    Skip TCP+auth handshake on every request by reusing a cached
    connection per PHP-FPM worker. No transactions/session vars/temp
    tables are used in this app, so no cross-request state leaks.

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -60,7 +60,8 @@ $container->set('db', function ($c) {
     return new PDO(
         "mysql:dbname={$config['db']['database']};host={$config['db']['host']};port={$config['db']['port']};charset=utf8mb4",
         $config['db']['username'],
-        $config['db']['password']
+        $config['db']['password'],
+        [PDO::ATTR_PERSISTENT => true]
     );
 });
 
