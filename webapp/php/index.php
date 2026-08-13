2f04c16d3fd767e6950a55f800fac84105a68e86
高木さん <t+takagi@mino.ltd>
2026-08-13 15:10:35 +0900

    perf: admin-users-projection-01-retry

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -509,7 +509,7 @@ $app->get('/admin/banned', function (Request $request, Response $response) {
     }
 
     $db = $this->get('db');
-    $ps = $db->prepare('SELECT * FROM `users` WHERE `authority` = 0 AND `del_flg` = 0 ORDER BY `created_at` DESC');
+    $ps = $db->prepare('SELECT `id`, `account_name` FROM `users` WHERE `authority` = 0 AND `del_flg` = 0 ORDER BY `created_at` DESC');
     $ps->execute();
     $users = $ps->fetchAll(PDO::FETCH_ASSOC);
 
